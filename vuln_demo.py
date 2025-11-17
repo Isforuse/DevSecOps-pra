b8a89fdcd24e33f5ba705f7e4860ab5394b9dbef
ww123 <154050812+Isforuse@users.noreply.github.com>
2025-11-17 15:06:04 +0800

    Update vuln_demo.py

--- vuln_demo.py
+++ vuln_demo.py
@@ -1,7 +1,20 @@
 import yaml
 import subprocess
+import hashlib
 
-def bad():
-    yaml.load("dangerous", Loader=yaml.Loader)
-    subprocess.call("rm -rf /", shell=True)
-    eval("print('bad')")
+def bad1():
+    eval("print('Hello')")
+
+def bad2():
+    yaml.load("foo")  # unsafe load
+
+def bad3(cmd):
+    subprocess.call(cmd, shell=True)
+
+def bad4(pwd):
+    hashlib.md5(pwd.encode()).hexdigest()
+
+bad1()
+bad2()
+bad3("ls; rm -rf /")
+bad4("mypassword")
